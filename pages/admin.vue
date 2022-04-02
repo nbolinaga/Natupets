a1d36cee6e16087f7bbe3fa601688a3be970b8ff
nbolinaga18 <nbolinaga17@gmail.com>
2022-04-02 19:18:06 -0400

    eslint fix

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,91 +1,91 @@
 <template>
-  <div class="p-10 grid gap-12 grid-cols-1 md:grid-cols-2 items-start">
-    <Price class="" />
+	<div class="p-10 grid gap-12 grid-cols-1 md:grid-cols-2 items-start">
+		<Price class="" />
 
-    <Stock class="" />
-    <ToVerified class="" />
-    <OrderList class="" :pedidos="porEntregar" titulo="POR ENTREGAR" />
-    <OrderList class="" :pedidos="entregados" titulo="ENTREGADOS" />
-    <OrderList class="" :pedidos="cancelados" titulo="CANCELADOS" />
-    <Twilio />
-  </div>
+		<Stock class="" />
+		<ToVerified class="" />
+		<OrderList class="" :pedidos="porEntregar" titulo="POR ENTREGAR" />
+		<OrderList class="" :pedidos="entregados" titulo="ENTREGADOS" />
+		<OrderList class="" :pedidos="cancelados" titulo="CANCELADOS" />
+		<Twilio />
+	</div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      pedidos: [],
-    }
-  },
-  computed: {
-    porEntregar() {
-      return this.pedidos.filter(
-        (pedido) => pedido.entregado === false && pedido.cancelado === false
-      )
-    },
-    cancelados() {
-      return this.pedidos.filter((pedido) => pedido.cancelado === true)
-    },
-    entregados() {
-      return this.pedidos.filter((pedido) => pedido.entregado === true)
-    },
-  },
-  beforeMount() {
-    if (this.$store.state.user.admin === false) {
-      this.$router.push('/')
-    }
-    const db = this.$fireModule.firestore()
-    const query = db.collection('pedidos').orderBy('fecha', 'asc')
-    query.onSnapshot((snapshot) => {
-      this.pedidos = []
-      snapshot.forEach((res) => {
-        const pedido = {
-          ...res.data(),
-          id: res.id,
-          cliente: res
-            .data()
-            .cliente.get()
-            .then((res) => {
-              pedido.cliente = res.data()
-            }),
-          fecha: this.getFormattedDate(res.data().fecha),
-          fechaPago: this.getFormattedDate(res.data().fechaPago),
-          fechaEntrega: this.getFormattedDate(res.data().fechaEntrega),
-          fechaCancelado: this.getFormattedDate(res.data().fechaCancelado),
-        }
-        this.pedidos.push(pedido)
-      })
-    })
-  },
-  methods: {
-    getFormattedDate(date) {
-      try {
-        date = date.toDate()
-        const year = date.getFullYear()
+	data() {
+		return {
+			pedidos: [],
+		}
+	},
+	computed: {
+		porEntregar() {
+			return this.pedidos.filter(
+				(pedido) => pedido.entregado === false && pedido.cancelado === false
+			)
+		},
+		cancelados() {
+			return this.pedidos.filter((pedido) => pedido.cancelado === true)
+		},
+		entregados() {
+			return this.pedidos.filter((pedido) => pedido.entregado === true)
+		},
+	},
+	beforeMount() {
+		if (this.$store.state.user.admin === false) {
+			this.$router.push('/')
+		}
+		const db = this.$fireModule.firestore()
+		const query = db.collection('pedidos').orderBy('fecha', 'asc')
+		query.onSnapshot((snapshot) => {
+			this.pedidos = []
+			snapshot.forEach((res) => {
+				const pedido = {
+					...res.data(),
+					id: res.id,
+					cliente: res
+						.data()
+						.cliente.get()
+						.then((res) => {
+							pedido.cliente = res.data()
+						}),
+					fecha: this.getFormattedDate(res.data().fecha),
+					fechaPago: this.getFormattedDate(res.data().fechaPago),
+					fechaEntrega: this.getFormattedDate(res.data().fechaEntrega),
+					fechaCancelado: this.getFormattedDate(res.data().fechaCancelado),
+				}
+				this.pedidos.push(pedido)
+			})
+		})
+	},
+	methods: {
+		getFormattedDate(date) {
+			try {
+				date = date.toDate()
+				const year = date.getFullYear()
 
-        let month = (1 + date.getMonth()).toString()
-        month = month.length > 1 ? month : '0' + month
+				let month = (1 + date.getMonth()).toString()
+				month = month.length > 1 ? month : '0' + month
 
-        let day = date.getDate().toString()
-        day = day.length > 1 ? day : '0' + day
+				let day = date.getDate().toString()
+				day = day.length > 1 ? day : '0' + day
 
-        return (
-          day +
-          '/' +
-          month +
-          '/' +
-          year +
-          ' - ' +
-          date.getHours() +
-          ':' +
-          date.getMinutes()
-        )
-      } catch (e) {
-        return ''
-      }
-    },
-  },
+				return (
+					day +
+					'/' +
+					month +
+					'/' +
+					year +
+					' - ' +
+					date.getHours() +
+					':' +
+					date.getMinutes()
+				)
+			} catch (e) {
+				return ''
+			}
+		},
+	},
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,27 @@
 <template>
-  <div class="p-10 h-screen overflow-hidden">
-    <p
-      class="text-2xl text-justify px-[3vw] md:px-[25vw] my-8 text-last-center"
-    >
-      Bienvenido a <span class="text-yellow-400 font-main">Natupets</span> esta
-      página es una prueba piloto para manejar la compra de la comida de tu
-      mascota, de manera fácil y rápida
-    </p>
-    <div class="text-center flex flex-col md:block mb-10 md:mb-4">
-      <button class="bg-[#501111] rounded-3xl px-4 ml-4 py-2 mb-6">
-        <nuxt-link to="/pedidos" class="text-white hover:text-yellow-400">
-          HACER UN PEDIDO
-        </nuxt-link>
-      </button>
-      <button class="bg-yellow-400 rounded-3xl px-4 ml-4 py-2">
-        <nuxt-link to="/perfil" class="text-white hover:text-[#501111]">
-          PERFIL
-        </nuxt-link>
-      </button>
-    </div>
-    <img
-      src="../static/dog.jpg"
-      alt="login con google"
-      class="w-[50vw] mx-[25vw] mb-8"
-    />
-  </div>
+	<div class="p-10 h-screen overflow-hidden">
+		<p class="text-2xl text-justify px-[3vw] md:px-[25vw] my-8 text-last-center">
+			Bienvenido a <span class="text-yellow-400 font-main">Natupets</span> esta
+			página es una prueba piloto para manejar la compra de la comida de tu
+			mascota, de manera fácil y rápida
+		</p>
+		<div class="text-center flex flex-col md:block mb-10 md:mb-4">
+			<button class="bg-[#501111] rounded-3xl px-4 ml-4 py-2 mb-6">
+				<nuxt-link to="/pedidos" class="text-white hover:text-yellow-400">
+					HACER UN PEDIDO
+				</nuxt-link>
+			</button>
+			<button class="bg-yellow-400 rounded-3xl px-4 ml-4 py-2">
+				<nuxt-link to="/perfil" class="text-white hover:text-[#501111]">
+					PERFIL
+				</nuxt-link>
+			</button>
+		</div>
+		<img
+			src="../static/dog.jpg"
+			alt="login con google"
+			class="w-[50vw] mx-[25vw] mb-8"/>
+	</div>
 </template>
 
 <script></script>
